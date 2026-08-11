7ae8d7fef76be1a6a35dbf173a2d019c6acb7479
Emilse Arias <emilsearias@gmail.com>
2026-08-11 10:41:05 -0300

    agregamos un state usuarioLogueado y lo guardo en el sessionStorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,18 @@ import Error404 from "./components/pages/Error404";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "./components/pages/Login";
 import FormularioServicio from "./components/pages/FormularioServicio";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('usuarioKey')) || false
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioSessionStorage)
+
+  useEffect(()=>{
+    sessionStorage.setItem('usuarioKey', JSON.stringify(usuarioLogueado) )
+  },[usuarioLogueado])
+
+
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-zinc-950 text-zinc-100 flex flex-col">
