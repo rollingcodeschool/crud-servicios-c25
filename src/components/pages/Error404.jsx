7919fd15e2e0788d6bdb3d6163220c4572c4115d
Emilse Arias <emilsearias@gmail.com>
2026-08-13 11:31:12 -0300

    agregue una imagen y estetica al componente Error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router";
+import logoError from "../../assets/404.png";
+
 const Error404 = () => {
-    return (
-        <div>
-            <h1>Pagina del error 404</h1>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img src={logoError} alt="logo del error 404" />
+      <p className="text-2xl ">Pagina no encontrada</p>
+      <Link to={"/"} className="text-blue-400 hover:underline">
+        Vuelve al inicio
+      </Link>
+    </div>
+  );
 };
 
 export default Error404;
